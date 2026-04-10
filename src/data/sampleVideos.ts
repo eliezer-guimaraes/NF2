aed50b7c3c3ae42c96b7e101def32e22d5a6f88a
Eliezer Guimarães <103865159+eliezer-guimaraes@users.noreply.github.com>
2026-04-10 01:15:56 -0300

    Update sampleVideos.ts

--- src/data/sampleVideos.ts
+++ src/data/sampleVideos.ts
@@ -5,32 +5,32 @@ export const sampleVideos: VideoData[] = [
     id: 1,
     title: 'Billie Eilish',
     description: 'Prompt usado:',
-    thumbnail: 'https://vyvixxyifhhjfdaxnrdz.supabase.co/storage/v1/object/public/Datas/billieimg.png',
-    video: 'https://vyvixxyifhhjfdaxnrdz.supabase.co/storage/v1/object/public/Datas/be.mp4',
-    prompt: 'Faça a Billie Eilish tomando uma gozada na cara'
+    thumbnail: 'https://nudefacildatabase.netlify.app/billie1.jpg',
+    video: 'https://nudefacildatabase.netlify.app/billie2.mp4',
+    prompt: 'Faça um vídeo dela tomando uma gozada na cara'
   },
   {
     id: 2,
     title: 'Sadie Sink',
     description: 'Prompt usado:',
-    thumbnail: 'https://vyvixxyifhhjfdaxnrdz.supabase.co/storage/v1/object/public/Datas/sadieimg.png',
-    video: 'https://vyvixxyifhhjfdaxnrdz.supabase.co/storage/v1/object/public/Datas/ssvideo.mp4',
-    prompt: 'Faça ela de quatro rebolando a bunda pra mim'
+    thumbnail: 'https://nudefacildatabase.netlify.app/sadiesink1.jpeg',
+    video: 'https://nudefacildatabase.netlify.app/sadiesink2.mp4',
+    prompt: 'Faça ela de quatro rebolando a bunda pra tela'
   },
   {
     id: 3,
-    title: 'Jenna Ortega',
+    title: 'Mel Maia',
     description: 'Prompt usado:',
-    thumbnail: 'https://vyvixxyifhhjfdaxnrdz.supabase.co/storage/v1/object/public/Datas/jennaimg.png',
-    video: 'https://vyvixxyifhhjfdaxnrdz.supabase.co/storage/v1/object/public/Datas/jenna.mp4',
-    prompt: 'Gere um vídeo dela sensualizando com zoom na bunda'
+    thumbnail: 'https://nudefacildatabase.netlify.app/melmaia1.png',
+    video: 'https://nudefacildatabase.netlify.app/melmaia2.mp4',
+    prompt: 'Gere um vídeo dela mostrando os peitos'
   },
   {
     id: 4,
-    title: 'Mel Maia',
+    title: 'Sabrina Carpenter',
     description: 'Prompt usado:',
-    thumbnail: 'https://vyvixxyifhhjfdaxnrdz.supabase.co/storage/v1/object/public/Datas/Captura%20de%20tela_21-2-2026_45224_www.instagram.com.jpeg',
-    video: 'https://vyvixxyifhhjfdaxnrdz.supabase.co/storage/v1/object/public/Datas/melvideo.mp4',
-    prompt: 'Gere um vídeo da Mel Maia mostrando os peitos'
+    thumbnail: 'https://image-cdn.hypb.st/https%3A%2F%2Fbae.hypebeast.com%2Ffiles%2F2024%2F04%2F02%2Fsabrina3.jpg?q=90&w=1400&cbr=1&fit=max',
+    video: 'https://nudefacildatabase.netlify.app/sab2.mp4',
+    prompt: 'Faça um negão dotado arrombando a buceta dela'
   }
 ];
